7c385c6e08d91416a38a99dbf69d2d65cf918e21
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-08-13 15:31:17 +0100

    Do not add empty lines at the end of output files.

--- tools/hcluster_sg_parser/hcluster_sg_parser.pl
+++ tools/hcluster_sg_parser/hcluster_sg_parser.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 
-#A simple perl parser to convert hcluster_sg 3-column output onto list of ids in separate files
+#A simple perl parser to convert hcluster_sg 3-column output into list of ids in separate files
 #hcluster_sg_parser.pl <file>
 #
 
@@ -12,18 +12,18 @@ my @table;
 my @cluster_ids;
 my $file1 = $ARGV[0];
 open my $fh1, '<', $file1;
-my $i=0;
+my $i = 0;
 
 #reads through file
 while (my $line = <$fh1>) {
+    chomp $line;
     my @row = split(/\t/, $line);
-   
+
     push @cluster_ids, $row[0];
-   
+
     my @list = split(/\,/, $row[2]);
     $table[$i] = [@list];
-   
-    
+
     $i++;
 }
 close $fh1;
@@ -32,13 +32,13 @@ $i = 0;
 
 #write into separate files with name of cluster_id
 foreach my $row (@table) {
-    open(my $fh, '>', $cluster_ids[$i]."_output.txt") or die "Could not open file '$i' $!";
-    
+    my $outfile = $cluster_ids[$i]."_output.txt";
+    open(my $fh, '>', $outfile) or die "Could not open file '$outfile' for writing: $!";
+
     foreach my $element (@$row) {
-        print $fh $element,"\n";
+        print $fh $element, "\n";
     }
-    
+
     close $fh;
     $i++;
 }
-
