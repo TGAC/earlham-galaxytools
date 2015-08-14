ae071042241081bcdb9acb9a703ee8ec19bc14bc
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-08-14 14:51:05 +0100

    Fix missing last element of the CIGAR string due to a bug in the for loop.

--- tools/TCoffee_to_CIGAR/TCoffee_to_cigar.pl
+++ tools/TCoffee_to_CIGAR/TCoffee_to_cigar.pl
@@ -2,53 +2,36 @@
 #
 use strict;
 use warnings;
-use List::Util qw(min max);
 
-
-#Git Copy
 #A simple perl parser to convert 2-column output where first column is fasta id and second is alignment sequence into 2-column output where first column is fasta id and second is CIGAR line
 #TCoffee_to_cigar.pl <file>
-#
-
 
 my $file1 = $ARGV[0];
 open my $fh1, '<', $file1;
 
 #reads through file
 while (my $line = <$fh1>) {
+    chomp $line;
     my @row = split(/\t/, $line);
-    my $cigar = "";
-    my @alignment = split("",$row[1]);
 
-    #converts gaps into D and match part into M
-    for (my $i=0; $i<$#alignment; $i++){
-        if($alignment[$i] eq '-'){
-            $cigar.= "D";
-        }else{
-            $cigar.= "M";
+    # Converts each match into M and each gap into D
+    my $long_cigar = $row[1];
+    $long_cigar =~ s/[^-]/M/g;
+    $long_cigar =~ s/-/D/g;
+
+    # Split the $long_cigar in substrings composed by the same letter
+    $long_cigar =~ s/DM/D,M/g;
+    $long_cigar =~ s/MD/M,D/g;
+    my @cigar_array = split(',', $long_cigar);
+
+    # Condense each substring, e.g. DDDD in 4D, and concatenate them again
+    my $cigar = '';
+    foreach my $str (@cigar_array) {
+        if (length($str) > 1) {
+            $cigar .= length($str);
         }
+        $cigar .= substr($str, 0, 1);
     }
-
-    my $find = "DM";
-    my $replace = "D,M";
-    $cigar =~ s/$find/$replace/g;
-    $find = "MD";
-    $replace = "M,D";
-    $cigar =~ s/'MD'/'M,D'/g;
-    $cigar =~ s/$find/$replace/g;
-
-    my @cigar_array = split(",",$cigar);
-
-    $cigar = "";
-
-    #converts string to CIGAR form with no of match and deletions
-    for (my $i=0; $i<$#cigar_array; $i++){
-        if(length($cigar_array[$i]) > 1){
-            $cigar.= length($cigar_array[$i]);
-        }
-        $cigar.= substr( $cigar_array[$i], 0 , 1 );
-    }
-    print "$row[0]\t$cigar\n";    
-
+    print "$row[0]\t$cigar\n";
 }
 close $fh1;
