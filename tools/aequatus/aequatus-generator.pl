93e2eff071e486bc8735ab6c396380303121afb2
anilthanki <thanki.anil@gmail.com>
2015-09-24 15:08:58 +0100

    XML adds requirement, perl adds comment to run command and restructures code

--- tools/aequatus/aequatus-generator.pl
+++ tools/aequatus/aequatus-generator.pl
@@ -3,23 +3,21 @@
 use strict;
 use warnings;
 
-use Bio::JSON;
-#A simple perl parser to convert hcluster_sg 3-column output into list of ids in separate files
-#hcluster_sg_parser.pl <file>
+use JSON;
+#A simple perl script to generate Aequatus from Newick from TreeBest best, Gene JSON file and CIGAR table format output of TCoffee-to-CIGAR
+#aequatus-generator.pl <tree-file> <Gene JSON file> <CIGAR file> 
 #
 
-my $out=[];
-
 my $tree = $ARGV[0];
-my $CIGAR = $ARGV[2];
 my $genes = $ARGV[1];
+my $CIGAR = $ARGV[2];
 
 my $tree_string = "";
 my $cigar_string = "";
 my %hash;
 open my $fh1, '<', $tree or die "Can't open '$tree': $!";
 
-#reads through file
+#reads through Tree file
 while (my $line = <$fh1>) {
     chomp($line);
 
@@ -27,16 +25,13 @@ while (my $line = <$fh1>) {
 }
 close $fh1;
 
-
-
 $hash{'tree'} = $tree_string;
 
-#print $json_text;
 open my $fh2, '<', $CIGAR or die "Can't open '$CIGAR': $!";;
 
 my %cigar_hash;
 
-#reads through file
+#reads through CIGAR file
 while (my $line = <$fh2>) {
     chomp($line);
     my @row = split(/\t/, $line);
@@ -44,26 +39,24 @@ while (my $line = <$fh2>) {
 }
 close $fh1;
 
-
-
-
 $hash{'cigar'} = \%cigar_hash;
 
 open my $fh3, '<', $genes or die "Can't open '$genes': $!";;
 
 my $genes_string = "";
-#reads through file
+
+#reads through Gene file
 while (my $line = <$fh3>) {
     chomp($line);
     $line =~ s/\s//g;
     $genes_string .= $line;
 }
 close $fh1;
-my $json = JSON->new->allow_nonref;
 
+my $json = JSON->new->allow_nonref;
 
 my $json_text = $json->decode($genes_string);
 
 $hash{'member'} = $json_text;
 
- print JSON->new->encode(\%hash);
+print JSON->new->encode(\%hash);
