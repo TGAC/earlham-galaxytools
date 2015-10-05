7841d728b4b36932aca469e343cfe920b3963fec
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-10-05 18:09:09 +0100

    Fixes for get_sequences tool.

---
+++ tools/Ensembl-REST/get_sequences/get_sequences.py
@@ -0,0 +1,36 @@
+# A simple tool to connect to the Ensembl server and retrieve sequences using
+# the Ensembl REST API.
+import json
+import optparse
+import requests
+from urlparse import urljoin
+
+server = "http://rest.ensembl.org"
+ext = "sequence/id"
+
+parser = optparse.OptionParser()
+parser.add_option('-i', '--input', dest="input_filename",
+                  help='List of Ensembl IDs')
+parser.add_option('-t', '--type', type='choice',
+                  choices=['genomic', 'cds', 'cdna', 'protein'],
+                  default="genomic", help='Type of sequence')
+parser.add_option('--expand_3prime', type='int', default=0,
+                  help='Expand the sequence downstream of the sequence by this many basepairs. Only available when using genomic sequence type')
+parser.add_option('--expand_5prime', type='int', default=0,
+                  help='Expand the sequence upstream of the sequence by this many basepairs. Only available when using genomic sequence type')
+options, args = parser.parse_args()
+if options.input_filename is None:
+    raise Exception('-i option must be specified')
+
+headers = {"Content-Type": "application/json", "Accept": "application/json"}
+params = dict((k, getattr(options, k)) for k in ['type', 'expand_3prime', 'expand_5prime'])
+with open(options.input_filename) as f:
+    ids = [line.strip() for line in f]
+data = {'ids': ids}
+r = requests.post(urljoin(server, ext), params=params, headers=headers,
+                  data=json.dumps(data))
+
+if not r.ok:
+    r.raise_for_status()
+
+print r.text
