a4aeb612abe64db7195095b1c5e792186ed35782
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-10-20 17:49:21 +0100

    Workaround 50 IDs limit. Return only FASTA. Misc fixes.

--- tools/Ensembl-REST/get_feature_info/get_feature_info.py
+++ tools/Ensembl-REST/get_feature_info/get_feature_info.py
@@ -5,25 +5,24 @@ import optparse
 import requests
 from urlparse import urljoin
 
-server = "http://rest.ensembl.org"
-ext = "lookup/id"
+server = 'http://rest.ensembl.org'
+ext = 'lookup/id'
 
 parser = optparse.OptionParser()
-parser.add_option('-i', '--input', dest="input_filename",
-                  help='List of Ensembl IDs')
+parser.add_option('-i', '--input', help='List of Ensembl IDs')
 parser.add_option('-e', '--expand', type='choice', choices=['0', '1'],
-                  default="0",
+                  default='0',
                   help='Expands the search to include any connected features. e.g. If the object is a gene, its transcripts, translations and exons will be returned as well.')
 parser.add_option('-f', '--format', type='choice',
-                  choices=['full', 'condensed'], default="full",
+                  choices=['full', 'condensed'], default='full',
                   help='Specify the formats to emit from this endpoint')
 options, args = parser.parse_args()
-if options.input_filename is None:
+if options.input is None:
     raise Exception('-i option must be specified')
 
-headers = {"Content-Type": "application/json", "Accept": "application/json"}
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 params = dict((k, getattr(options, k)) for k in ['format', 'expand'])
-with open(options.input_filename) as f:
+with open(options.input) as f:
     ids = [line.strip() for line in f]
 data = {'ids': ids}
 r = requests.post(urljoin(server, ext), params=params, headers=headers,
--- tools/Ensembl-REST/get_sequences/get_sequences.py
+++ tools/Ensembl-REST/get_sequences/get_sequences.py
@@ -3,42 +3,38 @@
 import json
 import optparse
 import requests
+from itertools import islice
 from urlparse import urljoin
 
-server = "http://rest.ensembl.org"
-ext = "sequence/id"
+server = 'http://rest.ensembl.org'
+ext = 'sequence/id'
 
 parser = optparse.OptionParser()
-parser.add_option('-i', '--input', dest="input_filename",
-                  help='List of Ensembl IDs')
+parser.add_option('-i', '--input', help='List of Ensembl IDs')
 parser.add_option('-t', '--type', type='choice',
                   choices=['genomic', 'cds', 'cdna', 'protein'],
-                  default="genomic", help='Type of sequence')
+                  default='genomic', help='Type of sequence')
 parser.add_option('--expand_3prime', type='int', default=0,
                   help='Expand the sequence downstream of the sequence by this many basepairs. Only available when using genomic sequence type')
 parser.add_option('--expand_5prime', type='int', default=0,
                   help='Expand the sequence upstream of the sequence by this many basepairs. Only available when using genomic sequence type')
-parser.add_option('-f', '--format', type='choice', 
-                  choices=['json', 'fasta'],
-                  default="fasta", help='Output type, can be either JSON or Fasta')
 options, args = parser.parse_args()
-if options.input_filename is None:
+if options.input is None:
     raise Exception('-i option must be specified')
 
-if options.format == "json":
-    headers = {"Content-Type": "application/json", "Accept": "application/json"}
-elif options.format == "fasta": 
-    headers = {"Content-Type": "text/x-fasta", "Accept": "text/x-fasta"}
-
-
+headers = {'Content-Type': 'text/x-fasta', 'Accept': 'text/x-fasta'}
 params = dict((k, getattr(options, k)) for k in ['type', 'expand_3prime', 'expand_5prime'])
-with open(options.input_filename) as f:
-    ids = [line.strip() for line in f]
-data = {'ids': ids}
-r = requests.post(urljoin(server, ext), params=params, headers=headers,
-                  data=json.dumps(data))
+with open(options.input) as f:
+    # Need to split the file in chunks of 50 lines because of the limit imposed by Ensembl
+    while True:
+        ids = [line.strip() for line in islice(f, 50)]
+        if not ids:
+            break
+        data = {'ids': ids}
+        r = requests.post(urljoin(server, ext), params=params, headers=headers,
+                          data=json.dumps(data))
 
-if not r.ok:
-    r.raise_for_status()
+        if not r.ok:
+            r.raise_for_status()
 
-print r.text
+        print r.text
