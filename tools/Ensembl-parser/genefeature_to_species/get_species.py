fa9443bc83bfd423e26eb02067eab3781120eb14
anilthanki <thanki.anil@gmail.com>
2016-06-01 16:32:50 +0100

    fixes error with travis

--- tools/Ensembl-parser/genefeature_to_species/get_species.py
+++ tools/Ensembl-parser/genefeature_to_species/get_species.py
@@ -3,23 +3,24 @@ import optparse
 
 species = []
 
-def readgene( gene ):
-	for transcript in gene['Transcript']:
-		species.append(transcript['species'])
+
+def readgene(gene):
+    for transcript in gene['Transcript']:
+        species.append(transcript['species'])
 
 parser = optparse.OptionParser()
 parser.add_option('-j', '--json', dest="input_gene_filename",
-					help='Gene Tree from Ensembl in JSON format')
+                help='Gene Tree from Ensembl in JSON format')
 
 options, args = parser.parse_args()
 
 if options.input_gene_filename is None:
-	raise Exception('-j option must be specified')
+    raise Exception('-j option must be specified')
 
-with open(options.input_gene_filename) as data_file:    
-	data = json.load(data_file)
+with open(options.input_gene_filename) as data_file:
+    data = json.load(data_file)
 
 for gene_dict in data.itervalues():
-	readgene(gene_dict)
+    readgene(gene_dict)
 
 print "\n".join(list(set(species)))
--- tools/Ensembl-parser/genetree_parser/genetree_parser.py
+++ tools/Ensembl-parser/genetree_parser/genetree_parser.py
@@ -3,55 +3,59 @@ import optparse
 
 species = []
 
-def speciesList( str ):
-	if not str['scientific_name'] in species:
-		speciesFile.write(str['scientific_name'])
-		species.append(str['scientific_name'])
-		speciesFile.write("\n")
 
-def geneLevel( str ):
-	if 'id' in str['sequence']:
-		proteinLevel(str['sequence'])
+def speciesList(str):
+    if not str['scientific_name'] in species:
+        speciesFile.write(str['scientific_name'])
+        species.append(str['scientific_name'])
+        speciesFile.write("\n")
 
-	if 'taxonomy' in str:
-		speciesList(str['taxonomy'])
 
-	gene_file.write(str['id']['accession'])
-	gene_file.write("\n")
-	return
+def geneLevel(str):
+    if 'id' in str['sequence']:
+        proteinLevel(str['sequence'])
 
-def proteinLevel( str ):
-	protein_file.write(str['id'][0]['accession'])
-	protein_file.write("\t")
-	protein_file.write(str['mol_seq']['cigar_line'])
-	protein_file.write("\n")
-	return
+    if 'taxonomy' in str:
+        speciesList(str['taxonomy'])
 
-def recursiveGenes( str ):
-	children_len = len(data['tree']['children'])
-	children_len -= 1
-	children = str['children']
+    gene_file.write(str['id']['accession'])
+    gene_file.write("\n")
+    return
 
-	while children_len >= 0:
-		if 'sequence' in children[children_len]: 
-			geneLevel(children[children_len])
-		else:
-			recursiveGenes(children[children_len])
-		children_len -= 1
-	return
+
+def proteinLevel(str):
+    protein_file.write(str['id'][0]['accession'])
+    protein_file.write("\t")
+    protein_file.write(str['mol_seq']['cigar_line'])
+    protein_file.write("\n")
+    return
+
+
+def recursiveGenes(str):
+    children_len = len(data['tree']['children'])
+    children_len -= 1
+    children = str['children']
+
+    while children_len >= 0:
+        if 'sequence' in children[children_len]: 
+            geneLevel(children[children_len])
+        else:
+            recursiveGenes(children[children_len])
+        children_len -= 1
+    return
 
 
 parser = optparse.OptionParser()
 parser.add_option('-i', '--input', dest="input_filename",
-					help='Gene Tree from Ensembl in JSON format')
+                    help='Gene Tree from Ensembl in JSON format')
 
 options, args = parser.parse_args()
 
 if options.input_filename is None:
-	raise Exception('-i option must be specified')
+    raise Exception('-i option must be specified')
 
 with open(options.input_filename) as data_file:
-	data = json.load(data_file)
+    data = json.load(data_file)
 
 children = data['tree']['children']
 children_len = len(data['tree']['children'])
@@ -62,9 +66,10 @@ protein_file = open("protein.tabular", 'w')
 speciesFile = open("species.txt", 'w')
 
 while children_len >= 0:
-	if 'sequence' in children[children_len]: 
-		geneLevel(children[children_len])
-	else:
-		recursiveGenes(children[children_len])
+    if 'sequence' in children[children_len]: 
+        geneLevel(children[children_len])
+    else:
+        recursiveGenes(children[children_len])
 
-	children_len -= 1
+    children_len -= 1
+    
