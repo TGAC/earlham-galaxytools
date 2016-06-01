cf195e0d0da23395eb7b58baea21eea950f21901
anilthanki <thanki.anil@gmail.com>
2016-06-01 11:37:10 +0100

    adds function to create species file

--- tools/Ensembl-parser/genetree_parser/genetree_parser.py
+++ tools/Ensembl-parser/genetree_parser/genetree_parser.py
@@ -1,51 +1,57 @@
 import json
-from pprint import pprint
-
 import optparse
-import requests
-from urlparse import urljoin
 
-def printme( str ):
+species = []
+
+def speciesList( str ):
+	if not str['scientific_name'] in species:
+		speciesFile.write(str['scientific_name'])
+		species.append(str['scientific_name'])
+		speciesFile.write("\n")
+
+def geneLevel( str ):
 	if 'id' in str['sequence']:
-		printid(str['sequence'])
+		proteinLevel(str['sequence'])
+
+	if 'taxonomy' in str:
+		speciesList(str['taxonomy'])
+
 	gene_file.write(str['id']['accession'])
 	gene_file.write("\n")
-	
 	return
 
-def printid( str ):
+def proteinLevel( str ):
 	protein_file.write(str['id'][0]['accession'])
 	protein_file.write("\t")
 	protein_file.write(str['mol_seq']['cigar_line'])
 	protein_file.write("\n")
-
 	return
 
-def recursiveprintme( str ):
+def recursiveGenes( str ):
 	children_len = len(data['tree']['children'])
 	children_len -= 1
 	children = str['children']
 
 	while children_len >= 0:
 		if 'sequence' in children[children_len]: 
-			printme(children[children_len])
+			geneLevel(children[children_len])
 		else:
-			recursiveprintme(children[children_len])
+			recursiveGenes(children[children_len])
 		children_len -= 1
 	return
 
 
 parser = optparse.OptionParser()
 parser.add_option('-i', '--input', dest="input_filename",
-                  help='Gene Tree from Ensembl in JSON format')
+					help='Gene Tree from Ensembl in JSON format')
 
 options, args = parser.parse_args()
 
 if options.input_filename is None:
-    raise Exception('-i option must be specified')
+	raise Exception('-i option must be specified')
 
-with open(options.input_filename) as data_file:    
-    data = json.load(data_file)
+with open(options.input_filename) as data_file:
+	data = json.load(data_file)
 
 children = data['tree']['children']
 children_len = len(data['tree']['children'])
@@ -53,11 +59,12 @@ children_len -= 1
 
 gene_file = open("gene.txt", 'w')
 protein_file = open("protein.tabular", 'w')
+speciesFile = open("species.txt", 'w')
 
 while children_len >= 0:
-    if 'sequence' in children[children_len]: 
-        printme(children[children_len])
-    else:
-    	recursiveprintme(children[children_len])
-    
-    children_len -= 1
+	if 'sequence' in children[children_len]: 
+		geneLevel(children[children_len])
+	else:
+		recursiveGenes(children[children_len])
+
+	children_len -= 1
