38bf72643a8eb3aa18340ed64128b528027f0ff7
anilthanki <thanki.anil@gmail.com>
2016-06-08 17:03:12 +0100

    fixes reports from GitHib and adds option to update NCBI database, writes output to file instead of STDOUT

--- tools/ete/ete_tree_generator.py
+++ tools/ete/ete_tree_generator.py
@@ -4,9 +4,6 @@ import optparse
 
 ncbi = NCBITaxa()
 
-species_name = []
-taxid = []
-
 parser = optparse.OptionParser()
 parser.add_option('-s', '--species', dest="input_species_filename",
                   help='Species list in text format one species in each line')
@@ -17,7 +14,16 @@ parser.add_option('-f', '--format', type='choice', choices=['0', '1', '2', '3', 
 parser.add_option('-t', '--treebest', type='choice', choices=['yes', 'no'], dest="treebest",
                   default='no', help='To be used in TreeBest')
 
+parser.add_option('-d', '--database', type='choice', choices=['yes','no'], dest="database",
+                  default='no', help='Update database')
+
 options, args = parser.parse_args()
+
+if options.database  == "yes":
+    try:
+        ncbi.update_taxonomy_database()
+    except:
+        pass
 
 if options.input_species_filename is None:
     raise Exception('-s option must be specified, Species list in text format one species in each line')
@@ -33,23 +39,24 @@ for index, species in enumerate(species_name):
 
 name2taxid = ncbi.get_name_translator(species_name)
 
-for species in species_name:
-    taxid.append(name2taxid[species][0])
+taxid = [name2taxid[_][0] for _ in species_name]
 
 tree = ncbi.get_topology(taxid)
 
-if(options.treebest == "yes"):
+if options.treebest == "yes":
     inv_map = {str(v[0]): k.replace(" ", "") + "*" for k, v in name2taxid.items()}
 else:
     inv_map = {str(v[0]): k for k, v in name2taxid.items()}
 
 
 for leaf in tree:
-    leaf.add_features(name=inv_map.get(leaf.name, "none"))
+    leaf.name = inv_map[leaf.name]
 
-newickTree = tree.write(format=int(options.format)).replace(";", "")
+newickTree = tree.write(format=int(options.format))
 
-if(options.treebest == "yes"):
+if options.treebest == "yes":
+    newickTree = newickTree.rstrip(';')
     newickTree = newickTree + "root;"
 
-print newickTree
+newickFile = open("newickTree.nhx", 'w')
+newickFile.write(newickTree)
