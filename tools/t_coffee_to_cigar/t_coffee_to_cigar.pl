4542b0ddfe5bafd70e9cafeee86caf0a5419c4c2
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2016-10-27 19:30:24 +0100

    Change t_coffee_to_cigar to accept FASTA instead of tabular
    Fix #36.

--- tools/t_coffee_to_cigar/t_coffee_to_cigar.pl
+++ tools/t_coffee_to_cigar/t_coffee_to_cigar.pl
@@ -3,26 +3,19 @@
 use strict;
 use warnings;
 
-#A simple perl parser to convert 2-column output where first column is fasta id and second is alignment sequence into 2-column output where first column is fasta id and second is CIGAR line
-#TCoffee_to_cigar.pl <file>
+# A simple Perl script to convert FASTA sequence alignments into 2-column output where first column is FASTA id and second is CIGAR line
+# TCoffee_to_cigar.pl <file>
 
-my $file1 = $ARGV[0];
-open my $fh1, '<', $file1;
-
-#reads through file
-while (my $line = <$fh1>) {
-    chomp $line;
-    my @row = split(/\t/, $line);
-
+sub convert_and_print {
+    my ($header, $sequence) = @_;
     # Converts each match into M and each gap into D
-    my $long_cigar = $row[1];
-    $long_cigar =~ s/[^-]/M/g;
-    $long_cigar =~ s/-/D/g;
+    $sequence =~ s/[^-]/M/g;
+    $sequence =~ s/-/D/g;
 
-    # Split the $long_cigar in substrings composed by the same letter
-    $long_cigar =~ s/DM/D,M/g;
-    $long_cigar =~ s/MD/M,D/g;
-    my @cigar_array = split(',', $long_cigar);
+    # Split the sequence in substrings composed by the same letter
+    $sequence =~ s/DM/D,M/g;
+    $sequence =~ s/MD/M,D/g;
+    my @cigar_array = split(',', $sequence);
 
     # Condense each substring, e.g. DDDD in 4D, and concatenate them again
     my $cigar = '';
@@ -32,6 +25,24 @@ while (my $line = <$fh1>) {
         }
         $cigar .= substr($str, 0, 1);
     }
-    print "$row[0]\t$cigar\n";
+    print "$header\t$cigar\n";
+}
+
+my $file1 = $ARGV[0];
+open my $fh1, '<', $file1;
+
+my $header = '', my $sequence = '';
+while (my $line = <$fh1>) {
+    chomp $line;
+    if (substr($line, 0, 1) eq '>') {
+        if ($header) {
+            convert_and_print($header, $sequence);
+        }
+        $header = substr($line, 1);
+        $sequence = '';
+    } else {
+        $sequence .= $line;
+    }
 }
 close $fh1;
+convert_and_print($header, $sequence);
