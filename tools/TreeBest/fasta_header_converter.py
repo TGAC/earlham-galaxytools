b7595e4125fceab6b696298480830300e619f2f5
anilthanki <thanki.anil@gmail.com>
2016-12-12 17:10:39 +0000

    fixes Planemo error

--- tools/TreeBest/fasta_header_converter.py
+++ tools/TreeBest/fasta_header_converter.py
@@ -1,15 +1,15 @@
 import json
 
 import optparse
-import requests
+
 
 transcript_species_dict = dict()
 sequence_dict = dict()
 
 
-def readgene( gene ):
-	for transcript in gene['Transcript']:
-		transcript_species_dict[transcript['id']] =  transcript['species'].replace("_","")
+def readgene(gene):
+    for transcript in gene['Transcript']:
+        transcript_species_dict[transcript['id']] = transcript['species'].replace("_", "")
 
 
 def read_fasta(fp):
@@ -17,17 +17,19 @@ def read_fasta(fp):
     for line in fp:
         line = line.rstrip()
         if line.startswith(">"):
-            if name: 
-            	sequence_dict[name] = ''.join(seq)
+            if name:
+                sequence_dict[name] = ''.join(seq)
             name, seq = line.replace(">", ""), []
         else:
             seq.append(line)
-    if name: 
+    if name:
         sequence_dict[name] = ''.join(seq)
 
+
 def add_species():
-	for id in sequence_dict:
-		print ">"+id+"_"+transcript_species_dict[id]+"\n"+sequence_dict[id]+"\n"
+    for id in sequence_dict:
+        print ">" + id + "_" + transcript_species_dict[id] + "\n" + sequence_dict[id] + "\n"
+
 
 parser = optparse.OptionParser()
 parser.add_option('-j', '--json', dest="input_gene_filename",
@@ -44,11 +46,11 @@ if options.input_gene_filename is None:
 if options.input_fasta_filename is None:
     raise Exception('-f option must be specified')
 
-with open(options.input_gene_filename) as data_file:    
+with open(options.input_gene_filename) as data_file:
     data = json.load(data_file)
 
 for gene_dict in data.itervalues():
-	readgene(gene_dict)
+    readgene(gene_dict)
 
 with open(options.input_fasta_filename) as fp:
     read_fasta(fp)
