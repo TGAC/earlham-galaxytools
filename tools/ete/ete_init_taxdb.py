fce848969954e956885caf2756d4a7a775145cce
Matthias Bernt <m.bernt@ufz.de>
2017-09-19 23:36:07 +0200

    implemented a taxdump update that works
    - also foor the currently available version of ete3
    - improved (but still dysfunctional tests)

--- tools/ete/ete_init_taxdb.py
+++ tools/ete/ete_init_taxdb.py
@@ -2,11 +2,11 @@ import optparse
 import sys
 # sys.path.append("./ete/")
 
-import ete3
+import ete3.ncbi_taxonomy
 
 parser = optparse.OptionParser()
 parser.add_option('-t', '--taxdump', dest='taxdump', default=None,
-                  help='NCBI taxdump (tar.gz)')
+                  help='NCBI taxdump (tar.gz) will be downloaded if not given')
 parser.add_option('-d', '--database', dest="database", default=None,
                   help='ETE sqlite data base to use (default: ~/.etetoolkit/taxa.sqlite)')
 options, args = parser.parse_args()
@@ -19,8 +19,8 @@ else:
     urlretrieve("http://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz", "taxdump.tar.gz")
     taxdump = "taxdump.tar.gz"
 
-try:
-    ete3.NCBITaxa(taxdump_file=taxdump, dbfile=options.database)
-except TypeError:
-    sys.stderr.write("ete_init_taxdb: outdated version of ete3")
-    exit(1)
+# will remove a taxdump.tar.gz file at the end 
+# which will lead to an errmessage if not present
+# if the tool is run on a taxdump in the current dir it will be 
+# deleted in the end
+ete3.ncbi_taxonomy.ncbiquery.update_db(dbfile=options.database, targz_file=taxdump)
