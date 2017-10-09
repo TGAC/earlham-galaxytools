30719706bb430b47f5af045f32e00194cb0f7a41
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2017-10-09 12:14:14 +0100

    Faster DELETE query. Use temporary file

--- tools/blast_parser/blast_parser.py
+++ tools/blast_parser/blast_parser.py
@@ -5,90 +5,90 @@ Simple parser to convert a BLAST 12-column or 24-column tabular output into a
 import argparse
 import math
 import sqlite3
+import tempfile
+
+BATCH_SIZE = 2000
 
 
 def create_tables(conn):
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE homology (
-        homology_id INTEGER NOT NULL,
+    cur.execute('''CREATE TABLE alignment (
+        id INTEGER PRIMARY KEY,
         sequence1_id VARCHAR NOT NULL,
         sequence2_id VARCHAR NOT NULL,
-        weight INT NOT NULL)''')
+        weight INTEGER NOT NULL)''')
     conn.commit()
 
 
 def main():
     parser = argparse.ArgumentParser()
-
     parser.add_argument('-i', metavar='in-file', type=argparse.FileType('rt'), required=True, help='Path to input file')
-
     parser.add_argument('-o', metavar='out-file', type=argparse.FileType('wt'), required=True, help='Path to output file')
-
     parser.add_argument('-r', action='store_true', default=False,
                         dest='reciprocal',
                         help='Annotate homolog pair')
-
-    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-
     options = parser.parse_args()
 
-    db_name = options.o.name.split(".")[0] + ".sqlite"
+    db_file = tempfile.NamedTemporaryFile(suffix=".sqlite")
 
-    conn = sqlite3.connect(db_name)
+    conn = sqlite3.connect(db_file.name)
     conn.execute('PRAGMA foreign_keys = ON')
 
     create_tables(conn)
 
-    i = 0
-
     cur = conn.cursor()
 
+    i = 0
     for line in options.i:
         line = line.rstrip()
         line_cols = line.split('\t')
         sequence1_id = line_cols[0]
         sequence2_id = line_cols[1]
-        evalue = float(line_cols[10])
 
         # Ignore self-matching hits
-        if sequence1_id != sequence2_id:
-            i = i + 1
+        if sequence1_id == sequence2_id:
+            continue
 
-            # Convert evalue to an integer weight with max 100
+        i = i + 1
+        evalue = float(line_cols[10])
+
+        # Convert evalue to an integer weight with max 100
+        if evalue != 0.0:
+            weight = min(100, round(math.log10(evalue) / -2.0))
+        else:
+            # If the evalue is 0, leave weight at 100
             weight = 100
 
-            # If the evalue is 0, leave weight at 100
-            if evalue != 0.0:
-                weight = min(100, round(math.log10(evalue) / -2.0))
+        cur.execute('INSERT INTO alignment (id, sequence1_id, sequence2_id, weight) VALUES (?, ?, ?, ?)',
+                    (i, sequence1_id, sequence2_id, weight))
 
-            # Insert pair into SQLite database
-            cur.execute('INSERT INTO homology (homology_id, sequence1_id, sequence2_id, weight) VALUES (?, ?, ?, ?)',
-                        (i, sequence1_id, sequence2_id, weight))
+        # Commit transaction at every BATCH_SIZE rows to save memory
+        if i % BATCH_SIZE == 0:
+            conn.commit()
+    # Commit final transaction
+    conn.commit()
+    options.i.close()
 
-            # Execute query at every 100 pair to save memory
-            if(i % 100 == 0):
-                conn.commit()
-
-    # Execute query at last
+    # Delete alternative alignments keeping only one with max weight
+    cur.execute('DELETE FROM alignment WHERE id NOT IN (SELECT id FROM alignment GROUP BY sequence1_id, sequence2_id HAVING weight=max(weight))')
     conn.commit()
 
-    # Delete duplicate keeping one with max weight
-    cur.execute('delete FROM homology WHERE homology_id  IN (SELECT a1.homology_id FROM homology a1 INNER JOIN homology a2 ON a1.sequence1_id = a2.sequence1_id AND a1.sequence2_id = a2.sequence2_id AND a1.weight < a2.weight)')
-
-    # Execute delete query
-    conn.commit()
-
-    # General select query
-    query = 'SELECT * FROM homology ORDER BY homology_id'
-
-    # Update select query if reciprocal selected
     if options.reciprocal:
-        query = 'SELECT h1.* FROM homology h1, homology h2 WHERE h1.sequence1_id =  h2.sequence2_id AND h1.sequence2_id =  h2.sequence1_id ORDER BY h1.homology_id'
+        query = 'SELECT a1.sequence1_id, a1.sequence2_id, a1.weight FROM alignment a1, alignment a2 WHERE a1.sequence1_id = a2.sequence2_id AND a1.sequence2_id = a2.sequence1_id ORDER BY a1.id'
+    else:
+        query = 'SELECT sequence1_id, sequence2_id, weight FROM alignment ORDER BY id'
 
     cur.execute(query)
-    results = cur.fetchall()
-    for result in results:
-        options.o.write("%s\t%s\t%d\n" % (result[1], result[2], result[3]))
+    while True:
+        rows = cur.fetchmany(BATCH_SIZE)
+        if not rows:
+            break
+        for row in rows:
+            options.o.write("%s\t%s\t%d\n" % row)
+
+    conn.close()
+    db_file.close()
+    options.o.close()
 
 
 if __name__ == "__main__":
