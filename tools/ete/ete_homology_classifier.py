69cf9afef434075eff1b5e5906ac479c799ec234
anilthanki <thanki.anil@gmail.com>
2018-05-05 14:07:19 +0100

    applies suggested changes

--- tools/ete/ete_homology_classifier.py
+++ tools/ete/ete_homology_classifier.py
@@ -23,15 +23,16 @@ def main():
     leaves_list = genetree.get_leaf_names()
 
     species_list = []
-    gene_lose_list = []
 
     for i, val in enumerate(leaves_list):
         if '_' in leaves_list[i]:
             species_list.append(leaves_list[i].split("_")[1])
-        else:
-            gene_lose_list.append(i)
 
-    leaves_list = [i for j, i in enumerate(leaves_list) if j not in gene_lose_list]
+    #Genetree nodes are required to be in gene_species format
+    leaves_list = [_ for _ in leaves_list if '_' in _]
+    for leaf in leaves_list:
+        species_list.append(leaf.split("_")[1])
+
 
     species_dict = {}
 
@@ -41,7 +42,7 @@ def main():
             count = "many"
         species_dict[val] = count
 
-    homologis = {
+    homologies = {
         'one-to-one': [],
         'one-to-many': [],
         'many-to-one': [],
@@ -55,23 +56,23 @@ def main():
             id1 = leaves_list[i].split(":")[1] if ":" in leaves_list[i] else leaves_list[i]
             id2 = leaves_list[j].split(":")[1] if ":" in leaves_list[j] else leaves_list[j]
             if(id1.split("_")[1] == id2.split("_")[1]):
-                homologis["paralogs"].append({id1: id2})
+                homologies["paralogs"].append({id1: id2})
             else:
-                homologis[species_dict[id1.split("_")[1]] + "-to-" + species_dict[id2.split("_")[1]]].append({id1: id2})
+                homologies[species_dict[id1.split("_")[1]] + "-to-" + species_dict[id2.split("_")[1]]].append({id1: id2})
 
     options.filters = options.filters.split(",")
 
     if options.out_format == 'tabular':
-        for homology, homologs in homologis.items():
+        for homology, homologs in homologies.items():
             # checks if homology type is in filter
             if homology in options.filters:
-                for element in homologis[homology]:
+                for element in homologies[homology]:
                     for key, value in element.items():
                         print(key + "\t" + value + "\t" + homology)
 
     elif options.out_format == 'csv':
         flag = False
-        for homology, homologs in homologis.items():
+        for homology, homologs in homologies.items():
             if len(homologs) > 0 and homology in options.filters:
                 flag = True
 
