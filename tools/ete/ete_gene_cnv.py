f344f8cc8a42c8b8f7db62c938d9bdb42d61159c
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2019-10-12 01:33:05 +0100

    Use tabular format for ete_gene_cnv output
    Cleanup Python script.

--- tools/ete/ete_gene_cnv.py
+++ tools/ete/ete_gene_cnv.py
@@ -6,14 +6,11 @@ import collections
 from ete3 import PhyloTree
 
 
-def printCSV(myDict, speciesList, colList=None):
+def printTSV(myDict, colList=None):
     """ Pretty print a list of dictionaries (myDict) as a dynamically sized table.
     If column names (colList) aren't specified, they will show in random order.
     Author: Thierry Husson - Use it as you want but don't blame me.
     """
-    colList = ["gene"]
-    colList.extend(speciesList)
-
     if not colList:
         colList = list(myDict[0].keys() if myDict else [])
 
@@ -23,63 +20,55 @@ def printCSV(myDict, speciesList, colList=None):
         myList.append([str(item[col] if item[col] is not None else '') for col in colList])
 
     for item in myList:
-        print(*item, sep=", ")
+        print(*item, sep="\t")
 
 
 def main():
     parser = argparse.ArgumentParser(description='Gene Copy Number Finder')
-    parser.add_argument('--genetree', help='GeneTree in nhx format')
-    parser.add_argument('--speciesorder', help='Comma separated species list')
+    parser.add_argument('--genetree', required=True, help='GeneTree in nhx format')
+    parser.add_argument('--speciesorder', required=True, help='Comma-separated species list')
     args = parser.parse_args()
 
-    if args.genetree is None:
-        parser.error("--genetree option must be specified, GeneTree in nhx format")
-
-    if args.speciesorder is None:
-        parser.error("--speciesorder option must be specified, Comma separated species list")
-
-    f = open(args.genetree, "r")
-
-    speciesList = args.speciesorder.split(",")
-
+    species_list = args.speciesorder.split(",")
+    species_list = [_.strip() for _ in species_list]
     table = []
 
-    # reads multiple gene tree line by line gene tree
-    for row in f:
+    with open(args.genetree, "r") as f:
+        # reads multiple gene tree line by line gene tree
+        for line in f:
+            # Remove empty NHX features that can be produced by TreeBest but break ete3
+            line = line.replace('[&&NHX]', '')
 
-        contents = row
+            # reads single gene tree
+            genetree = PhyloTree(line)
+            leaves = genetree.get_leaf_names()
 
-        # Remove empty NHX features that can be produced by TreeBest but break ete3
-        contents = contents.replace('[&&NHX]', '')
+            leaves_parts = [_.split("_") for _ in leaves]
+            for i, leaf_parts in enumerate(leaves_parts):
+                if len(leaf_parts) != 2:
+                    raise Exception("Leaf node '%s' is not in gene_species format" % leaves[i])
 
-        # reads single gene tree
-        genetree = PhyloTree(contents)
+            leaves_species = [_[1] for _ in leaves_parts]
+            species_counter = collections.Counter(leaves_species)
 
-        leaves = genetree.get_leaf_names()
+            # Assign to ref_species the first element of species_list which
+            # appears in a leaf node
+            for ref_species in species_list:
+                if ref_species in species_counter:
+                    break
+            else:
+                raise Exception("None of the specified species was found in the GeneTree '%s'" % line)
 
-        # Genetree nodes are required to be in gene_species format
-        leaves_list = [_ for _ in leaves if '_' in _]
-
-        ref_species = speciesList[0]
-
-        species_list = [_.split("_")[1] for _ in leaves]
-
-        counter = collections.Counter(species_list)
-
-        if counter[ref_species] == 0:
-            for s in speciesList:
-                if counter[s] > 0:
-                    ref_species = s
+            # Find the gene of the (first) leaf node for the ref_species
+            for leaf_parts in leaves_parts:
+                if leaf_parts[1] == ref_species:
+                    species_counter['gene'] = leaf_parts[0]
                     break
 
-        for x in leaves_list:
-            if x.split("_")[1] == ref_species and counter[ref_species] > 0:
-                counter['gene'] = x.split("_")[0]
-                break
+            table.append(species_counter)
 
-        table.append(counter)
-
-    printCSV(table, speciesList)
+    colList = ["gene"] + species_list
+    printTSV(table, colList)
 
 
 if __name__ == "__main__":
