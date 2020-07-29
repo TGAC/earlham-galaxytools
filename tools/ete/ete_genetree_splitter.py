eab409212e784485177af2cea890540cae9379a5
Anil Thanki (EI) <thankia@n140518.nbi.ac.uk>
2020-07-29 14:21:05 +0100

    separated gainlose adds option to choose between split_by_dups or treeko algorithm

--- tools/ete/ete_genetree_splitter.py
+++ tools/ete/ete_genetree_splitter.py
@@ -11,8 +11,8 @@ def main():
     parser.add_option('--genetree', help='GeneTree in nhx format')
     parser.add_option('--speciestree', help='Species Tree in nhx format')
     parser.add_option('--species_format', type='int', default=8, help='Species Tree input format (0-9)')
+    parser.add_option('--split', help='Split method')
     parser.add_option('--gene_node', type='int', default=0, help='Gene node format 0=gene_species, 1=species_gene')
-    parser.add_option('--gainlose', action='store_true', default=False, help='Find out gene gain/lose')
     parser.add_option('--output_format', type='int', default=9, help='GeneTree output format (0-9)')
     options, args = parser.parse_args()
 
@@ -32,27 +32,20 @@ def main():
     if options.gene_node == 0:
         genetree.set_species_naming_function(parse_sp_name)
 
-    # reconcile species tree with gene tree to help find out gene gain/lose
-    if options.gainlose:
+    if options.split == "dups":
+        # splits tree by duplication events which returns the list of all subtrees resulting from splitting current tree by its duplication nodes.
+        for cluster_id, node in enumerate(genetree.split_by_dups(), 1):
+            outfile = str(cluster_id) + '_genetree.nhx'
+            with open(outfile, 'w') as f:
+                f.write(node.write(format=options.output_format))
 
-        if options.speciestree is None:
-            parser.error("--speciestree option must be specified, species tree in nhx format")
-
-        # reads species tree
-        speciestree = PhyloTree(options.speciestree, format=options.species_format)
-
-        # Removes '*' from Species names comes from Species tree configrured for TreeBest
-        for leaf in speciestree:
-            leaf.name = leaf.name.strip('*')
-
-        genetree, events = genetree.reconcile(speciestree)
-
-    # splits tree by duplication events which returns the list of all subtrees resulting from splitting current tree by its duplication nodes.
-    for cluster_id, node in enumerate(genetree.split_by_dups(), 1):
-        outfile = str(cluster_id) + '_genetree.nhx'
-        with open(outfile, 'w') as f:
-            f.write(node.write(format=options.output_format))
-
+    elif options.split == "treeKO":
+        # splits tree by using speciation tree generated from genetree utilising treeKO tool.
+        ntrees, ndups, sptrees = genetree.get_speciation_trees()
+        for cluster_id, spt in enumerate(sptrees, 1):
+            outfile = str(cluster_id) + '_genetree.nhx'
+            with open(outfile, 'w') as f:
+                f.write(spt.write(format=options.output_format))
 
 def parse_sp_name(node_name):
     return node_name.split("_")[1]
