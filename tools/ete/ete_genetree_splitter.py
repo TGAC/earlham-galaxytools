68751c5ee0ca941bba4e4e160e3a59b806e73ce6
Anil Thanki (EI) <thankia@n140518.nbi.ac.uk>
2020-10-19 17:59:36 +0100

    Making travis happy

--- tools/ete/ete_genetree_splitter.py
+++ tools/ete/ete_genetree_splitter.py
@@ -13,8 +13,7 @@ def main():
     parser.add_option('--species_format', type='int', default=8, help='Species Tree input format (0-9)')
     parser.add_option('--gene_node', type='int', default=0, help='Gene node format 0=gene_species, 1=species_gene')
     parser.add_option('--gainlose', action='store_true', default=False, help='Find out gene gain/lose')
-    parser.add_option('--split', type='choice', choices=['dups', 'treeko'], dest="split",
-                  default='dups', help='Choose GeneTree splitting algorithms')
+    parser.add_option('--split', type='choice', choices=['dups', 'treeko'], dest="split", default='dups', help='Choose GeneTree splitting algorithms')
     parser.add_option('--output_format', type='int', default=9, help='GeneTree output format (0-9)')
     options, args = parser.parse_args()
 
@@ -57,15 +56,14 @@ def main():
                 f.write(node.write(format=options.output_format))
     elif options.split == "treeko":
         # splits tree by using TreeKO algorithms.
-        ntrees, ndups, sptrees =  genetree.get_speciation_trees()
+        ntrees, ndups, sptrees = genetree.get_speciation_trees()
 
         cluster_id = 0
         for spt in sptrees:
-            cluster_id = cluster_id + 1;
+            cluster_id = cluster_id + 1
             outfile = str(cluster_id) + '_genetree.nhx'
             with open(outfile, 'w') as f:
                 f.write(spt.write(format=options.output_format))
-
 
 
 def parse_sp_name(node_name):
