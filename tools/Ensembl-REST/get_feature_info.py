9d8d9238bcf3d63c7ddce88e936d5996fd958fda
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2025-02-17 13:00:26 +0000

    Fix get_genetree for Ensembl REST API changes
    See https://www.ensembl.info/2023/06/09/changes-to-endpoints-of-ensembl-compara-apis/
    
    Also:
    - Add citation
    - Move scripts to Python 3.13
    - Lint code with black

--- tools/Ensembl-REST/get_feature_info.py
+++ tools/Ensembl-REST/get_feature_info.py
@@ -1,37 +1,45 @@
 # A simple tool to connect to the Ensembl server and retrieve feature
 # information using the Ensembl REST API.
-from __future__ import print_function
-
 import json
 import optparse
 from itertools import islice
+from urllib.parse import urljoin
 
 import requests
-from six.moves.urllib.parse import urljoin
 
 parser = optparse.OptionParser()
-parser.add_option('-i', '--input', help='List of Ensembl IDs')
-parser.add_option('-e', '--expand', type='choice', choices=['0', '1'],
-                  default='0',
-                  help='Expands the search to include any connected features. e.g. If the object is a gene, its transcripts, translations and exons will be returned as well.')
+parser.add_option("-i", "--input", help="List of Ensembl IDs")
+parser.add_option(
+    "-e",
+    "--expand",
+    type="choice",
+    choices=["0", "1"],
+    default="0",
+    help="Expands the search to include any connected features. e.g. If the object is a gene, its transcripts, translations and exons will be returned as well.",
+)
 
-parser.add_option('-f', '--format', type='choice',
-                  choices=['full', 'condensed'], default='full',
-                  help='Specify the formats to emit from this endpoint')
+parser.add_option(
+    "-f",
+    "--format",
+    type="choice",
+    choices=["full", "condensed"],
+    default="full",
+    help="Specify the formats to emit from this endpoint",
+)
 options, args = parser.parse_args()
 if options.input is None:
-    raise Exception('-i option must be specified')
+    raise Exception("-i option must be specified")
 
 
-server = 'https://rest.ensembl.org'
-ext = 'lookup/id'
+server = "https://rest.ensembl.org"
+ext = "lookup/id"
 
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-params = dict((k, getattr(options, k)) for k in ['format', 'expand'])
+headers = {"Content-Type": "application/json", "Accept": "application/json"}
+params = {k: getattr(options, k) for k in ("format", "expand")}
 
 first = True
 
-print('{')
+print("{")
 
 with open(options.input) as f:
     while True:
@@ -40,9 +48,14 @@ with open(options.input) as f:
             break
         if not first:
             print(",")
-        data = {'ids': ids}
-        r = requests.post(urljoin(server, ext), params=params, headers=headers,
-                          data=json.dumps(data), allow_redirects=False)
+        data = {"ids": ids}
+        r = requests.post(
+            urljoin(server, ext),
+            params=params,
+            headers=headers,
+            data=json.dumps(data),
+            allow_redirects=False,
+        )
 
         if not r.ok:
             r.raise_for_status()
@@ -51,4 +64,4 @@ with open(options.input) as f:
 
         first = False
 
-print('}')
+print("}")
