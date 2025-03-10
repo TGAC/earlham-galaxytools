bfb33031e1c6aa21cf050ad7b4744c9d27e27e50
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2025-03-10 13:20:30 +0000

    Update to ete3 3.1.3
    Also:
    - Add sqlite 3.40 requirement to workaround https://github.com/etetoolkit/ete/pull/770
    - Update test syntax

--- tools/ete/ete_init_taxdb.py
+++ tools/ete/ete_init_taxdb.py
@@ -1,24 +1,32 @@
 import optparse
+from urllib.request import urlretrieve
 
-import ete3.ncbi_taxonomy
-from six.moves.urllib.request import urlretrieve
+from ete3.ncbi_taxonomy.ncbiquery import update_db
 
 parser = optparse.OptionParser()
-parser.add_option('-t', '--taxdump', dest='taxdump', default=None,
-                  help='NCBI taxdump (tar.gz) will be downloaded if not given')
-parser.add_option('-d', '--database', dest="database", default=None,
-                  help='ETE sqlite data base to use (default: ~/.etetoolkit/taxa.sqlite)')
+parser.add_option(
+    "-t",
+    "--taxdump",
+    dest="taxdump",
+    default=None,
+    help="NCBI taxdump (tar.gz), will be downloaded if not given",
+)
+parser.add_option(
+    "-d",
+    "--database",
+    dest="database",
+    default=None,
+    help="ETE sqlite data base to create",
+)
 options, args = parser.parse_args()
 if options.database is None:
     parser.error("-d option must be specified")
 if options.taxdump is not None:
     taxdump = options.taxdump
 else:
-    urlretrieve("https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz", "taxdump.tar.gz")
+    urlretrieve(
+        "https://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz", "taxdump.tar.gz"
+    )
     taxdump = "taxdump.tar.gz"
 
-# will remove a taxdump.tar.gz file at the end
-# which will lead to an errmessage if not present
-# if the tool is run on a taxdump in the current dir it will be
-# deleted in the end
-ete3.ncbi_taxonomy.ncbiquery.update_db(dbfile=options.database, targz_file=taxdump)
+update_db(dbfile=options.database, targz_file=taxdump)
